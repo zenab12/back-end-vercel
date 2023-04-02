91f52fc72f8d1e92b7e2276312ed3ab24fdab0b2
Zien Mohammed Abdelmonaem <78083890+zenab12@users.noreply.github.com>
2023-04-02 14:52:24 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
 
 server.use(middlewares)
 // Add this before server.use(router)
@@ -10,7 +12,22 @@ server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
-server.use(router)
+server.db = router.db;
+server.use(auth);
+server.use(router);
+
+
+server.post('/posts', (req, res) => {
+  const db = router.db;
+  const { title, body,imageUrl,videoUrl,tags,userId } = req.body;
+  const id = Date.now().toString();
+  tags = tags.split(",");
+  const post = { id, title, body,imageUrl,videoUrl,tags,userId };
+  db.get('posts').push(post).write();
+  res.json(post);
+});
+
+
 server.listen(3000, () => {
     console.log('JSON Server is running')
 })
